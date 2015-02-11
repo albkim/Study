cfe1d0e2f13f93ec8b68e8da400e9e246ca4621b
Su Yang <alsubang@gmail.com>
2015-02-11 02:03:54 -0800

    Add integer partition

---
+++ Algorithms.Implementation/IntegerPartition.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Algorithms.Implementation
+{
+    public class IntegerPartition
+    {
+
+        public static List<List<int>> GetPartition(int number)
+        {
+            List<List<int>> partitions = new List<List<int>>();
+
+            GetPartition(4, 4, partitions, new List<int>());
+
+            return partitions;
+        }
+
+        /// <summary>
+        /// we want to get partitions like this for sum = 4
+        /// 4
+        /// 3 + 1
+        /// 2 + 2
+        /// 2 + 1 + 1
+        /// 1 + 1 + 1 + 1
+        /// </summary>
+        /// <param name="sum"></param>
+        /// <param name="largestNumber"></param>
+        /// <param name="partitions"></param>
+        private static void GetPartition(int sum, int largestNumber, List<List<int>> partitions, List<int> currentParitition)
+        {
+            if (sum <= 0)
+            {
+                //we crossed the end, we should finish this partition
+                //3. add to partition 1 + 1 + 1 + 1
+                //8. add to partition 2 + 1 + 1
+                //10. add to partition 2 + 2
+                //15. add to partition 3 + 1
+                //19. add to partition 4
+                partitions.Add(currentParitition);
+                return;
+            }
+
+            if (largestNumber > 1)
+            {
+                //this will go
+                //1. have 4, 4 -> 4, 3 -> 4, 2 -> 4, 1
+                //4. process 4, 2
+                //6. have 2, 2 -> 2, 1
+                //8. process 2, 2
+                //11. process 4, 3
+                //13. have 1, 3 -> 1, 2 -> 1, 1
+                //16. process 1, 2 -> process 1, 3
+                //17. process 4, 4
+                GetPartition(sum, largestNumber - 1, partitions, new List<int>());
+            }
+            
+            if (sum >= largestNumber)
+            {
+                //first we will have 4, 1, so we will have
+                //2. add 1 -> 3, 1 -> add 1 -> 2, 1 -> add 1 -> 1, 1 -> add 1 -> 0, 1
+                //5. add 2 -> 2, 2
+                //7. add 1 -> 1, 1 -> add 1 -> 0, 1
+                //9. add 2 -> 1, 2 -> add 2 -> -1, 2
+                //12. add 3 -> 1, 3 
+                //14. add 1 -> 0, 1
+                //18. add 4 -> 0, 4
+                currentParitition.Add(largestNumber);
+                GetPartition(sum - largestNumber, largestNumber, partitions, currentParitition);
+            }
+        }
+
+    }
+}
